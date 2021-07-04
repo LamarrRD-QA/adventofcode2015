22e0ec4fdf3be2af2f069fbcaba9c75c01106e01
Lamarr Redhead-Davis <LRedhead-Davis@qa.com>
2021-07-04 15:17:53 +0100

    LRD: Create command line runner

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import sys
+from more_itertools import partition
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def validate_list(elem):
+    if ':' in elem:
+        [lhs_str, rhs_str] = elem.split(':', 1)
+        try:
+            lhs = int(lhs_str)
+            rhs = int(rhs_str)
+        except ValueError:
+            return False
+        if 1 <= lhs <= 25 and 0 <= rhs <= 2:
+            return True
+    else:
+        try:
+            day = int(elem)
+        except ValueError:
+            return False
+        if 1 <= day <= 25:
+            return True
 
 
-# Press the green button in the gutter to run the script.
+def determine_runner():
+    temp_list = filter(validate_list, sys.argv[1:])
+    singles, pairs = partition(lambda token: ':' in token, temp_list)
+
+    runner = dict(x.split(':') for x in pairs)
+    runner = {int(k): int(v) for k, v in runner.items()}
+    for x in singles:
+        runner[int(x)] = 0
+    print(runner)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    determine_runner()
