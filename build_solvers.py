2e0d7ca183e06ff613e606ef3e6071bbe63e0b68
Lamarr Redhead-Davis <LRedhead-Davis@qa.com>
2021-07-05 19:53:53 +0100

    LRD: Add day 4 to solvers list

--- build_solvers.py
+++ build_solvers.py
@@ -1,5 +1,6 @@
 from src.day1.solutions import DayOne
 from src.day2.solutions import DayTwo
 from src.day3.solutions import DayThree
+from src.day4.solutions import DayFour
 
-SOLVERS = [DayOne, DayTwo, DayThree]
+SOLVERS = [DayOne, DayTwo, DayThree, DayFour]
