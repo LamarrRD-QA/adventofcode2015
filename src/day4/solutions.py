efc8965f15f5852ea8a20e35d9bf0fb50e101d12
Lamarr Redhead-Davis <LRedhead-Davis@qa.com>
2021-07-05 19:53:37 +0100

    LRD: Implement solutions for day 4

---
+++ src/day4/solutions.py
@@ -0,0 +1,32 @@
+import hashlib
+import itertools
+
+from src.solution_template import Solver
+
+
+class DayFour(Solver):
+    FILENAME = 'resources/input_day4.txt'
+
+    def solve_part1(self):
+        f = open(self.FILENAME)
+        data = f.read().rstrip()
+        f.close()
+
+        for num in itertools.count(start=1):
+            temp = "{}{}".format(data, num).encode('utf-8')
+            m = hashlib.md5(temp)
+            if m.hexdigest().startswith('00000'):
+                print("Solution for Day 4, Part 1 is: {}".format(num))
+                break
+
+    def solve_part2(self):
+        f = open(self.FILENAME)
+        data = f.read().rstrip()
+        f.close()
+
+        for num in itertools.count(start=1):
+            temp = "{}{}".format(data, num).encode('utf-8')
+            m = hashlib.md5(temp)
+            if m.hexdigest().startswith('000000'):
+                print("Solution for Day 4, Part 2 is: {}".format(num))
+                break
